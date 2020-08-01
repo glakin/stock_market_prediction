db3bdcc3c0bb702586f4d25d1a0e96369d3c20a8
glakin <gerardlakin@gmail.com>
2020-08-01 18:34:52 -0500

    changed it into a function

--- fetch_intraday.py
+++ fetch_intraday.py
@@ -8,7 +8,8 @@ Created on Sat Aug  1 15:33:49 2020
 
 from alpha_vantage.timeseries import TimeSeries
 
-ts = TimeSeries(key='MU7WOKE2G7L93MXA', output_format='pandas')
-data, meta_data = ts.get_intraday('GOOGL', outputsize='full')
-
-data.to_csv('./GOOGL_intraday.csv')
+def fetch_intraday(symbol):
+    # Should come up with a system to handle the KPI key
+    ts = TimeSeries(key='MU7WOKE2G7L93MXA', output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol, outputsize='full')
+    data.to_csv('./{}_intraday.csv'.format(symbol, meta_data))

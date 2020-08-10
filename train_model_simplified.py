311b52c834c49a4f858355aa2db1d60df8d9a16f
glakin <gerardlakin@gmail.com>
2020-08-09 22:56:36 -0500

    finished generating predicted close values. added plots. fixed an error that was causing the model to train on negative values

--- train_model_simplified.py
+++ train_model_simplified.py
@@ -7,70 +7,72 @@ from os import path
 from datetime import date
 from sklearn import preprocessing
 
-current_date = date.today()
-sample_days = 50
+current_date=date.today()
+sample_days=50
 
 # Stock symbol to train the model with
-symbol = "KO"
+symbol="NFLX"
 
 # Load technicals and price data
 # Write/read from csv to reduce API calls
 if path.exists("./data/{}_technicals_{}.csv".format(symbol, current_date)):
-    technicals = pd.read_csv("./data/{}_technicals_{}.csv".format(symbol, current_date))
+    technicals=pd.read_csv("./data/{}_technicals_{}.csv".format(symbol, current_date))
 else:
-    technicals = fetch_technicals(symbol, interval = "daily", save_csv = True)
+    technicals=fetch_technicals(symbol, interval="daily", save_csv=True)
     time.sleep(60)
 if path.exists("./data/{}_daily_{}.csv".format(symbol, current_date)):
-    prices = pd.read_csv("./data/{}_daily_{}.csv".format(symbol, current_date))
+    prices=pd.read_csv("./data/{}_daily_{}.csv".format(symbol, current_date))
+    prices=prices.set_index("date", drop=True)
 else:
-    prices = fetch_daily(symbol, save_csv = True)
+    prices=fetch_daily(symbol, save_csv=True)
 
 # Build dataset
 
-df = prices.set_index('date', drop=True)
-df.index.names = ['date']
+df=prices
+df.index.names=['date']
+df=df.sort_index()
 
-input_data = df.to_numpy()
-days_history = len(input_data)
-ncols = len(df.columns)
+input_data=df.to_numpy()
+days_history=len(input_data)
+ncols=len(df.columns)
 
 # Build a target dataset of the change in price between closes
-close = input_data[:,3][:]
-close_change = np.ndarray(shape=(days_history,1), dtype=float)
+close=input_data[:,3][:]
+close_change=np.ndarray(shape=(days_history,1), dtype=float)
 for i in range(days_history-1):
-    close_change[i] = close[i]-close[i+1]
-close_change[days_history-1] = 0
+    close_change[i]=close[i+1]-close[i]
+close_change[days_history-1]=0
 
 #Set the training/test split
-test_split = 0.9 
-n = int(input_data.shape[0] * test_split)
+test_split=0.9 
+n=int(input_data.shape[0] * test_split)
 
-input_train = input_data[:n,:]
-input_test = input_data[n:,:]
+input_train=input_data[:n,:]
+input_test=input_data[n:,:]
 
-close_change_train = close_change[:n]
-close_change_test = close_change[n:]
+close_change_train=close_change[:n]
+close_change_test=close_change[n:]
 
 # Create scalers for train/test subsets of input/target data
-scaler_input_train = preprocessing.StandardScaler()
-scaler_input_test = preprocessing.StandardScaler()
-scaler_target_train = preprocessing.StandardScaler()
-scaler_target_test = preprocessing.StandardScaler()
+scaler_input_train=preprocessing.StandardScaler()
+scaler_input_test=preprocessing.StandardScaler()
+scaler_target_train=preprocessing.StandardScaler()
+scaler_target_test=preprocessing.StandardScaler()
 
 # Normalize the data
-input_train_norm = scaler_input_train.fit_transform(input_train)
-input_test_norm = scaler_input_test.fit_transform(input_test)
-target_train_norm = scaler_target_train.fit_transform(close_change_train)
-target_test_norm = scaler_target_test.fit_transform(close_change_test)
+input_train_norm=scaler_input_train.fit_transform(input_train)
+input_test_norm=scaler_input_test.fit_transform(input_test)
+target_train_norm=scaler_target_train.fit_transform(close_change_train)
+target_test_norm=scaler_target_test.fit_transform(close_change_test)
 
 # Split the input and target data into groupings of size sample_days
-X_train = np.array([input_train_norm[i:i + sample_days].copy() for i in range(len(input_train_norm) - sample_days)])
-X_test = np.array([input_test_norm[i:i + sample_days].copy() for i in range(len(input_test_norm) - sample_days)])
+X_train=np.array([input_train_norm[i:i + sample_days].copy() for i in range(len(input_train_norm) - sample_days)])
+X_test=np.array([input_test_norm[i:i + sample_days].copy() for i in range(len(input_test_norm) - sample_days)])
 
-y_train = np.array([target_train_norm[i + sample_days].copy() for i in range(len(target_train_norm) - sample_days)])
-#y_train = np.expand_dims(y_train, -1)
-y_test = np.array([target_test_norm[i + sample_days].copy() for i in range(len(target_test_norm) - sample_days)])
-#y_test = np.expand_dims(y_test, -1)
+y_train=np.array([target_train_norm[i + sample_days].copy() for i in range(len(target_train_norm) - sample_days)])
+#y_train=np.expand_dims(y_train, -1)
+y_test=np.array([target_test_norm[i + sample_days].copy() for i in range(len(target_test_norm) - sample_days)])
+#y_test=np.expand_dims(y_test, -1)
 
 # Model
 import keras
@@ -83,16 +85,16 @@ np.random.seed(4)
 #from tensorflow import set_random_seed
 #set_random_seed(4)
 
-lstm_input = Input(shape=(sample_days, ncols), name='lstm_input')
-x = LSTM(50, name='lstm_0')(lstm_input)
-x = Dropout(0.2, name='lstm_dropout_0')(x)
-x = Dense(64, name='dense_0')(x)
-x = Activation('sigmoid', name='sigmoid_0')(x)
-x = Dense(1, name='dense_1')(x)
-output = Activation('linear', name='linear_output')(x)
-model = Model(inputs=lstm_input, outputs=output)
+lstm_input=Input(shape=(sample_days, ncols), name='lstm_input')
+x=LSTM(50, name='lstm_0')(lstm_input)
+x=Dropout(0.2, name='lstm_dropout_0')(x)
+x=Dense(64, name='dense_0')(x)
+x=Activation('sigmoid', name='sigmoid_0')(x)
+x=Dense(1, name='dense_1')(x)
+output=Activation('linear', name='linear_output')(x)
+model=Model(inputs=lstm_input, outputs=output)
 
-adam = optimizers.Adam(lr=0.0005)
+adam=optimizers.Adam(lr=0.0005)
 
 model.compile(optimizer=adam, loss='mse')
 
@@ -101,13 +103,30 @@ model.compile(optimizer=adam, loss='mse')
 #plot_model(model, to_file='model.png')
 
 model.fit(x=X_train, y=y_train, batch_size=32, epochs=50, shuffle=True, validation_split=0.1)
-evaluation = model.evaluate(X_test, y_test)
+evaluation=model.evaluate(X_test, y_test)
 print(evaluation)
 
-# Code runs but the model is returning null
-y_test_predicted = model.predict(X_test)
+y_test_predicted=model.predict(X_test)
 
-y_test_predicted = scaler_target_test.inverse_transform(y_test_predicted)
-y_train = scaler_target_test.inverse_transform(y_train[:,0,0])
+y_test_predicted=scaler_target_test.inverse_transform(y_test_predicted)
+y_test=scaler_target_test.inverse_transform(y_test)
+
+dates=df.index[-len(y_test_predicted)+1:]
+dates2=df.index[-len(y_test_predicted):]
+
+actual_close=close[-len(y_test_predicted)+1:] + y_test[:-1][1]
+pred_close=close[-len(y_test_predicted)+1:] + y_test_predicted[:-1][1]
+
+import matplotlib.pyplot as plt
+plt.plot(dates, actual_close, label='actual')
+plt.plot(dates, pred_close, label='predicted')
+plt.legend(['Actual','Predicted'])
+
+plt.plot(dates2, y_test, label='actual')
+plt.plot(dates2, y_test_predicted, label='predicted')
+plt.legend(['Actual','Predicted'])
+
+sum(y_test_predicted)
+sum(y_test)
 
 

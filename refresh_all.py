bb2172ac45df899c6f01dc17217a98de1598d4cb
glakin <gerardlakin@gmail.com>
2020-09-02 15:03:36 -0500

    refresh_all.py will refresh the data in the database as well as the models for each symbol in symbols.csv. The whole process takes several hours due to pauses between API calls and should probably be run at night.

---
+++ refresh_all.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from refresh_functions import refresh_data, refresh_models
+
+symbols = pd.read_csv('./data/symbols.csv')['symbol'].to_list()
+refresh_data(symbols)
+refresh_models(symbols)

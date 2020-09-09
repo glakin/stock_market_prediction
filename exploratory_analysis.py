0f63f8f74c478fd8e98f5de835d81b0adcb1112a
glakin <gerardlakin@gmail.com>
2020-09-08 22:17:37 -0500

    added new columns to the close change query - looking at 5-day changes in addition to 1-day. calculated slope of sma. looked at relationships and discovered that the sma value provided by alphavantage does not account for stock splits. need to either figure out how to change that or calculate sma directly.

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -40,11 +40,24 @@ query = '''
     '''.format(symbol)
     
 cc_query = '''
+    with prices as 
+        (
+          select symbol, 
+              date, 
+              adjusted_close, 
+              row_number() over (partition by symbol order by date asc) rownum 
+          from prices_daily  
+        )
+    
    	select p.date,
            p.symbol,
-   		   (p2.adjusted_close - p.adjusted_close)/p.adjusted_close close_change_pct               
-   	from (select symbol, date, adjusted_close, row_number() over (partition by symbol order by date asc) rownum from prices_daily) p
-   	join (select symbol, date, adjusted_close, row_number() over (partition by symbol order by date asc) rownum from prices_daily) p2 on p.symbol = p2.symbol and p2.rownum = p.rownum+1
+           p1.adjusted_close - p.adjusted_close close_change_1_day,
+   		   (p1.adjusted_close - p.adjusted_close)/p.adjusted_close close_change_pct_1_day,
+           p5.adjusted_close - p.adjusted_close close_change_5_day,
+   		   (p5.adjusted_close - p.adjusted_close)/p.adjusted_close close_change_pct_5_day
+   	from prices p
+   	join prices p1 on p.symbol = p1.symbol and p1.rownum = p.rownum+1
+    join prices p5 on p.symbol = p5.symbol and p5.rownum = p.rownum+5
    	left join earnings e on p.symbol = e.symbol and p.date = e.date
    	where e.date is null
     and p.symbol = '{}'
@@ -66,18 +79,40 @@ close_change = close_change[close_change.date >= min_date]
 
 df = pd.merge(features, close_change, on = ['symbol','date'])
 
-df['up_down'] = np.where(df['close_change_pct'] > 0, 'up', 'down')
+# Define whether the stock went up or down 
+df['up_down_1_day'] = np.where(df['close_change_pct_1_day'] > 0, 'up', 'down')
+df['up_down_5_day'] = np.where(df['close_change_pct_5_day'] > 0, 'up', 'down')
+
 df['close_sma25_delta'] = df['adjusted_close'] - df['sma_25']
 df['close_ema25_delta'] = df['adjusted_close'] - df['ema_25']
+df['sma_25_slope_1_day'] = df['sma_25'].diff()
+df['sma_25_slope_5_day'] = df['sma_25'].diff()/5
+df['sma_25_slope_direction_5_day'] = np.where(df['sma_25_slope_5_day'] > 0, 'up', 'down')
 
-feature = 'net_aroon'
+df.pivot_table(values = 'date',index = 'up_down_5_day', columns = 'sma_25_slope_direction_5_day', aggfunc = 'count')
+
+feature = 'sma_25_slope_5_day'
+feature2 = 'sma_25'
 
 plt.figure()
 sns.distplot(df[feature])
 
 plt.figure()
-sns.jointplot(x = feature, y = 'close_change_pct', data = df, kind = "hex")
+sns.violinplot(x = 'sma_25_slope_direction_5_day', y = 'close_change_pct_5_day', data = df)
 
 plt.figure()
-sns.violinplot(x = "up_down", y = feature, data = df)
+sns.jointplot(x = feature, y = 'close_change_pct_5_day', data = df, kind = "reg")
+
+plt.figure()
+sns.jointplot(x = feature2, y = 'close_change_pct_5_day', data = df, kind = "reg")
+
+# Filtering to move it past the July 2012 KO stock split
+df_filtered = df[df['date'] >= '2013-01-01']
+
+plt.figure()
+sns.jointplot(x = feature, y = 'close_change_pct_5_day', data = df_filtered, kind = "reg")
+
+plt.figure()
+sns.jointplot(x = feature2, y = 'close_change_pct_5_day', data = df_filtered, kind = "reg")
+
 

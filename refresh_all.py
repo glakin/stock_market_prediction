1825b3f9cf296bab3d59d8529d47ae51c9cc9472
glakin <gerardlakin@gmail.com>
2020-09-17 20:55:36 -0500

    added offset_days to refresh_models call

--- refresh_all.py
+++ refresh_all.py
@@ -3,4 +3,4 @@ from refresh_functions import refresh_data, refresh_models
 
 symbols = pd.read_csv('./data/symbols.csv')['symbol'].to_list()
 refresh_data(symbols)
-refresh_models(symbols)
+refresh_models(symbols, offset_days = 5)

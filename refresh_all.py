94ca425269b4c9f07dacdcb9d7c8bb773ef1902c
glakin <gerardlakin@gmail.com>
2020-10-29 22:15:20 -0500

    added comments. testing github connection

--- refresh_all.py
+++ refresh_all.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from refresh_functions import refresh_data, refresh_models
 
+# Read symbol list
 symbols = pd.read_csv('./data/symbols.csv')['symbol'].to_list()
+
+# Refresh the price and earnings data in the database
 refresh_data(symbols)
+
+# Refresh models with new data. Configure parameter offset_days to change
+# which price we are trying to predict
 refresh_models(symbols, offset_days = 5)

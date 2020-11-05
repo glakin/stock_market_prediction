da145a42f4eb6066c86cb7a333a2cdce1bfe4020
glakin <gerardlakin@gmail.com>
2020-11-04 21:01:30 -0600

    minor updates

--- quantstrat_practice.R
+++ quantstrat_practice.R
@@ -1,3 +1,6 @@
+# This is copied mostly from 
+# https://rdrr.io/github/braverock/quantstrat/f/vignettes/quantstrat-RFinance-2018.Rmd
+
 library(quantstrat)
 
 stock.str <- 'EEM'
@@ -10,8 +13,8 @@ initEq=100000
 portfolio.st='macd'
 account.st='macd'
 
-initPortf(portfolio.st,symbols=stock.str)
-initAcct(account.st,portfolios=portfolio.st,initEq = initEq)
+initPortf(portfolio.st, symbols=stock.str)
+initAcct(account.st,portfolios=portfolio.st, initEq=initEq)
 initOrders(portfolio=portfolio.st)
 
 strategy.st<-portfolio.st
@@ -20,6 +23,7 @@ strategy(strategy.st, store=TRUE)
 
 library(rjson)
 
+# read api key from credentials.json
 cred <- fromJSON(file = 'credentials.json')
 cred["tiingo"]
 
@@ -28,3 +32,36 @@ getSymbols(stock.str,
            adjust=TRUE,
            src='tiingo',
            api.key=cred["tiingo"])
+
+#MA parameters for MACD
+fastMA = 12 
+slowMA = 26 
+signalMA = 9
+maType="EMA"
+
+#one indicator
+add.indicator(strategy.st, name = "MACD", 
+              arguments = list(x=quote(Cl(mktdata)),
+                               nFast=fastMA, 
+                               nSlow=slowMA),
+              label='_' 
+)
+
+#two signals
+add.signal(strategy.st,
+           name="sigThreshold",
+           arguments = list(column="signal._",
+                            relationship="gt",
+                            threshold=0,
+                            cross=TRUE),
+           label="signal.gt.zero"
+)
+
+add.signal(strategy.st,
+           name="sigThreshold",
+           arguments = list(column="signal._",
+                            relationship="lt",
+                            threshold=0,
+                            cross=TRUE),
+           label="signal.lt.zero"
+)
